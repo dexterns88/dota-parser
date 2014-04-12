ba43ae2403809aa45be4a7a41c1116061b67c1c1
dexterns88 <dexterns88@gmail.com>
2014-04-13 01:35:39 +0200

    config change

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'dota.loc';
+$config['base_url']	=  'http://' . $_SERVER['SERVER_NAME'];
 
 /*
 |--------------------------------------------------------------------------
