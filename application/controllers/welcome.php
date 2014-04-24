95fbfc93c20e728046f9101d0263fff4b7253d71
dexterns88 <dexterns88@gmail.com>
2014-04-24 23:11:10 +0200

    update style

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -8,17 +8,19 @@ class Welcome extends MY_Controller {
 
     $this->load->model( array('save_replay' , 'news_modal' ) );
 
+    $this->data['body_class'] = "front";
+
   //home page view
     $query_pagination['page'] = 0;
     $query_pagination['item_per_page'] = 5;
 
-    $query['type'] = "block";
+    $query['type'] = "block block-replay";
 
     $query['lists'] = $this->save_replay->view_all( 'limit', $query_pagination );
 
     $news['config']['limit']['count'] = 5;
     $news['config']['limit']['start'] = 0;
-    $news['type'] = 'block';
+    $news['type'] = 'block block_news';
 
     $news['content'] = $this->news_modal->read( $news['config'] );
 
@@ -26,7 +28,10 @@ class Welcome extends MY_Controller {
 
     $query['title'] = "Latest saved game";
 
-    $this->data['content'] = $this->twig->render('list_view' , $query );
+
+    $this->data['content'] = "<div class='clear'>" . $this->twig->render('list_view' , $query );
+
+    $this->data['content'] .= "</div>";
 
     $this->data['content'] .= $this->twig->render('news_list' , $news );
 
