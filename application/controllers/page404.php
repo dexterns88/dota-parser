c76381a51da2d4cc3ab07f38ea5fa6432d91bab8
dota <dexterns88@gmail.com>
2014-05-22 11:05:22 +0200

    update controller 404

--- application/controllers/page404.php
+++ application/controllers/page404.php
@@ -1,16 +1,15 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 //class Welcome extends CI_Controller {
-class page404 extends MY_Controller {
+class page404 extends CI_Controller {
 
   public function index()
   {
-    $this->data['body_class'] = "page404";
     $this->data['content'] = "<h1>Page not found</h1>";
     $this->data['content'] .= "<h2>Content you're looking for is not here!!!</h2>";
     $this->data['content'] .= "<div class='hero-search'><img width='460px' src='/images/site/404_2.png' alt='404'></div>";
 
-    $this->twig->display('main_tpl' , $this->data );
+    $this->load->view('page404', $this->data);
   }
 
 }
