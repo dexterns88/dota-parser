1dcdb6fac041f341323f86c9a7df7e3b0efc2147
dexterns88 <dexterns88@gmail.com>
2014-10-06 01:27:24 +0200

    up

--- application/config/routes.php
+++ application/config/routes.php
@@ -41,8 +41,8 @@
 $route['(:any)'] = 'downtime';
 $route['default_controller'] = 'downtime';
 
-$route['default_controller'] = "welcome";
-$route['404_override'] = 'page404';
+//$route['default_controller'] = "welcome";
+//$route['404_override'] = 'page404';
 
 
 /* End of file routes.php */
